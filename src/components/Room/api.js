dc3aec5a0e202878c3de4a2ab61e61f939c91361
senhai <95114113+senhai-kun@users.noreply.github.com>
2023-05-30 18:21:49 +0800

    server link update

--- src/components/Room/api.js
+++ src/components/Room/api.js
@@ -1,3 +1,3 @@
-export const urlSocket = "https://senhai-connect-server.herokuapp.com/"
+export const urlSocket = "https://senhai-connect-server.onrender.com/"
 // https://senhai-connect-server.herokuapp.com/
 // http://localhost:4000/
